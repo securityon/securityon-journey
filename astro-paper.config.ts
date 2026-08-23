c487bb7ff6b8fb78b49ff6223a43c30d079e0e26
BAN Seok <securityon@gmail.com>
2026-08-23 13:42:48 +0900

    style: refine mobile layout and site identity

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -30,14 +30,23 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
 
-  socials: [],
+  socials: [
+    { name: "github",
+      url: "https://github.com/securityon",
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/securityon/",
+    },
+    {
+      name: "mail", url: "mailto:security@securityon.org",
+    },
+  ],
+  
 
   shareLinks: [
-    { name: "whatsapp", url: "https://wa.me/?text=" },
-    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
-    { name: "x", url: "https://x.com/intent/post?url=" },
-    { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
-    { name: "mail", url: "mailto:?subject=See%20this%20post&body=" },
+    { name: "mail",
+      url: "mailto:?subject=Research%20Note&body="
+    },
   ],
 });
